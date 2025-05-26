9ad883fd644208002e6f1d8751d88a05e948855a
esythir <dev.brunamassi@gmail.com>
2025-05-26 17:37:10 -0300

    fix(connectionfactory): add driver class

--- src/main/java/br/com/kofin/factory/ConnectionFactory.java
+++ src/main/java/br/com/kofin/factory/ConnectionFactory.java
@@ -5,9 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USER = "RM560535";
+    private static final String URL      = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private static final String USER     = "RM560535";
     private static final String PASSWORD = "101199";
+
+    static {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError("Oracle JDBC Driver not found: " + e.getMessage());
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);

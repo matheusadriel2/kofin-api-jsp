78d7005fda7f33b8274fb71f8cae5ec6e597480f
matheusadriel2 <matheusadrieldsc@gmail.com>
2025-08-09 18:20:06 -0300

    feat: adds daofactory

--- src/main/java/br/com/kofin/dao/DaoFactory.java
+++ src/main/java/br/com/kofin/dao/DaoFactory.java
@@ -1,4 +1,34 @@
 package br.com.kofin.dao;
 
-public class DaoFactory {
+import java.sql.SQLException;
+
+public final class DaoFactory {
+
+    private DaoFactory() {
+    }
+
+    public static CardsDao createCardsDao() throws SQLException {
+        return new CardsDao();
+    }
+
+    public static TransactionsDao createTransactionsDao() throws SQLException {
+        return new TransactionsDao();
+    }
+
+    public static AccountsDao createAccountsDao() throws SQLException {
+        return new AccountsDao();
+    }
+
+    public static UsersDao createUsersDao() throws SQLException {
+        return new UsersDao();
+    }
+
+    public static UserPasswordDao createUserPasswordDao() throws SQLException {
+        return new UserPasswordDao();
+    }
+
+    public static UserDetailsDao createUserDetailsDao() throws SQLException {
+        return new UserDetailsDao();
+    }
+
 }
